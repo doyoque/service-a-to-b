29cdf4eca9852f230d0dcb9c0592e55e0a4fd35f
doyoque <abiyoga1335@gmail.com>
2023-10-01 13:16:56 +0700

    feat: unit test integer & float value

--- service_util/util_test.go
+++ service_util/util_test.go
@@ -29,6 +29,42 @@ func TestGetOrDefault_Bool(t *testing.T) {
 	}
 }
 
+func TestGetOrDefault_Int(t *testing.T) {
+	envVariableName := "INT_VAR"
+	defaultValue := 42
+	os.Setenv(envVariableName, "99")
+	defer os.Unsetenv(envVariableName)
+
+	result := GetOrDefault(envVariableName, defaultValue)
+	if result != 99 {
+		t.Errorf("Expected GetOrDefault to return '99', but got '%d'", result)
+	}
+}
+
+func TestGetOrDefault_Float32(t *testing.T) {
+	envVariableName := "FLOAT32_VAR"
+	defaultValue := float32(1.2345)
+	os.Setenv(envVariableName, "3.14159")
+	defer os.Unsetenv(envVariableName)
+
+	result := GetOrDefault(envVariableName, defaultValue)
+	if result != float32(3.14159) {
+		t.Errorf("Expected GetOrDefault to return '3.14159', but got '%f'", result)
+	}
+}
+
+func TestGetOrDefault_Float64(t *testing.T) {
+	envVariableName := "FLOAT_VAR"
+	defaultValue := 3.14
+	os.Setenv(envVariableName, "2.71828")
+	defer os.Unsetenv(envVariableName)
+
+	result := GetOrDefault(envVariableName, defaultValue)
+	if result != 2.71828 {
+		t.Errorf("Expected GetOrDefault to return '2.71828', but got '%f'", result)
+	}
+}
+
 func TestGetOrDefault_DefaultValue(t *testing.T) {
 	envVariableName := "MISSING_VAR"
 	defaultValue := "default"
